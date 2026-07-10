6f9c8838e493ef75535b872a519a1868bcd71147
Aryan <aryangp291@gmail.com>
2026-07-10 20:15:28 +0000

    Add some rotation logic for turns

--- js/chess.js
+++ js/chess.js
@@ -5,3 +5,20 @@ let boardArray = board.childNodes;
 board.addEventListener("click", function (event) {
   console.log(event.target.parentNode);
 });
+
+let pieces = document.querySelectorAll("img");
+let turn = 0;
+
+function rotatePieces() {
+  if (turn) {
+    pieces.forEach(function (element) {
+      element.setAttribute("class", "rotate-180");
+    });
+  } else {
+    pieces.forEach(function (element) {
+      element.setAttribute("class", "rotate-0");
+    });
+  }
+
+  turn = turn ? 0 : 1;
+}
